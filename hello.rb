c62f2fae7d37c1925164a37d2c7b9b1148ee354b
shirosweets <60824319+shirosweets@users.noreply.github.com>
2020-07-04 04:25:03 -0300

    Update hello.rb

--- hello.rb
+++ hello.rb
@@ -1,3 +1,10 @@
 puts 'Hello, world!'
 puts ''
 puts 'Good-bye.'
+
+puts 'Bienvenido  a mis primeras pruebas del lenguaje de programacion Ruby :)'
+puts ''
+puts 'Puedes sumar de la siguiente manera: '
+puts '2+2'
+puts 2+2
+puts ''
